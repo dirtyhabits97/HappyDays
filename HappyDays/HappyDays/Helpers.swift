3d26a2fe3e426ba4e9f5e775fec29b73546af9b1
Gonzalo Reyes Huertas <gonzalorehu.97.31@gmail.com>
2017-06-18 21:07:37 -0700

    Basic functionality

--- HappyDays/HappyDays/Helpers.swift
+++ HappyDays/HappyDays/Helpers.swift
@@ -13,3 +13,27 @@ extension UIColor {
         self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
     }    
 }
+extension UIImage {
+    func resize(to width: CGFloat) -> UIImage? {
+        let scale = width / self.size.width
+        let height = self.size.height * scale
+        // create a new image context to draw into
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 0)
+        // draw the original image into the context
+        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        // pull out the resized version
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        // end the context so UIKit can clean up
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    func resize(scale: CGFloat) -> UIImage? {
+        let width = self.size.width * scale
+        let height = self.size.height * scale
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 0)
+        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+}
--- HappyDays/HappyDays/MemoriesViewController.swift
+++ HappyDays/HappyDays/MemoriesViewController.swift
@@ -15,17 +15,19 @@ class MemoriesViewController: UICollectionViewController {
     
     
     // MARK: - Object Ids
-    private let cellId = "cellId"
-    private let headerId = "headerId"
+    fileprivate let cellId = "cellId"
+    fileprivate let headerId = "headerId"
+    
+    // MARK: - Object Variables
+    var memories = [URL]()
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.barStyle = .black
-        navigationController?.navigationBar.isTranslucent = true
-        navigationController?.navigationBar.isHidden = true
+        setupNavBar()
         collectionView?.backgroundColor = .darkGray
         registerCells()
+        loadMemories()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,6 +45,13 @@ class MemoriesViewController: UICollectionViewController {
         collectionView?.register(MemoriesViewCell.self, forCellWithReuseIdentifier: cellId)
         collectionView?.register(MemoriesViewHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
     }
+    fileprivate func setupNavBar() {
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.tintColor = .white
+        navigationItem.title = "Memories"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(handleAddMemory))
+    }
     fileprivate func checkPermissions() {
         let photosAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
         let recordingAuthorized = AVAudioSession.sharedInstance().recordPermission() == .granted
@@ -54,12 +63,94 @@ class MemoriesViewController: UICollectionViewController {
         }
     }
     
-    // MARK: - CollectionView Methods
+    // MARK: - Handle Methods
+    
+    func handleAddMemory() {
+        selectImage()
+    }
+}
+
+// MARK: - Logic Methods
+
+extension MemoriesViewController {
+    enum MemoryType: String {
+        case jpg, thumb, m4a, txt
+        var fileExtension: String {
+            switch self {
+            case .jpg: return ".jpg"
+            case .thumb: return ".thumb"
+            case .m4a: return ".m4a"
+            case .txt: return ".txt"
+            }
+        }
+    }
+    // behaves as a refresh
+    fileprivate func loadMemories() {
+        memories.removeAll()
+        // attempt to load all the memories in our documents directory
+        guard let files = try? FileManager.default.contentsOfDirectory(at: getDocumentsDirectory(), includingPropertiesForKeys: nil, options: []) else { return }
+        for file in files {
+            // name of the component, e.g. url/     swag.thumb
+            let filename = file.lastPathComponent
+            if filename.hasSuffix(MemoryType.thumb.fileExtension) {
+                // get the root name of the memory, withouth its path extension
+                let noExtension = filename.replacingOccurrences(of: MemoryType.thumb.fileExtension, with: "")
+                // create a full path from the memory
+                let memoryPath = getDocumentsDirectory().appendingPathComponent(noExtension)
+                memories.append(memoryPath)
+            }
+        }
+        collectionView?.reloadSections(IndexSet(integer: 0))
+    }
+    fileprivate func saveNewMemory(image: UIImage) {
+        // create a unique name for this memory based on time so it's ez to sort
+        let memoryName = "memory-\(Date().timeIntervalSince1970)"
+        let imageName = memoryName + MemoryType.jpg.fileExtension
+        let thumbnailName = memoryName + MemoryType.thumb.fileExtension
+        do {
+            // create url with the names created
+            let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+            // convert the UIImage into a JPEG data object
+            if let imageJPEGData = UIImageJPEGRepresentation(image, 80) {
+                // write that data to the url created
+                try imageJPEGData.write(to: imagePath, options: [.atomic])
+            }
+            
+            if let thumbnail = image.resize(to: 200) {
+                let thumbnailPath = getDocumentsDirectory().appendingPathComponent(thumbnailName)
+                if let thumbnailJPEGData = UIImageJPEGRepresentation(thumbnail, 80) {
+                    try thumbnailJPEGData.write(to: thumbnailPath, options: [.atomic])
+                }
+            }
+        } catch {
+            print("Failed to save new memory")
+        }
+    }
+    fileprivate func getMemoryURL(for memory: URL, type: MemoryType) -> URL {
+        return memory.appendingPathExtension(type.rawValue)
+    }
+    fileprivate func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+}
+
+// MARK: - CollectionView Methods
+
+extension MemoriesViewController {
+    /*override func numberOfSections(in collectionView: UICollectionView) -> Int {
+     return 2
+     } */
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return /*section == 0 ? 0 :*/ memories.count
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! MemoriesViewCell
+        let memory = memories[indexPath.item]
+        let imageName = getMemoryURL(for: memory, type: .thumb).path
+        let image = UIImage(contentsOfFile: imageName)
+        cell.photoImageView.image = image
         return cell
     }
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
@@ -84,5 +175,26 @@ extension MemoriesViewController: UICollectionViewDelegateFlowLayout {
     // for cells
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 20
+    }
+}
+
+// MARK: - ImagePickerDelegate Methods
+
+extension MemoriesViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func selectImage() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.modalPresentationStyle = .formSheet
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            saveNewMemory(image: image)
+            loadMemories()
+        } else {
+            print("ImagePicker: Something went wrong")
+        }
+        dismiss(animated: true)
     }
 }
